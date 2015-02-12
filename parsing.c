19389770fc0c7414db670083dbd352f5470ea02c
Pedro Howat <pedro.howat@gmail.com>
2015-02-12 00:31:48 -0200

    Part of chapter 8

--- parsing.c
+++ parsing.c
@@ -23,6 +23,54 @@ void add_history(char * unused) {}
 #include <editline/history.h>
 #endif
 
+enum { LVAL_NUM, LVAL_ERR };
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+typedef struct {
+  int type;
+  long num;
+  int err;
+} lval;
+
+/* Creates new number type lval */
+lval lval_num(long x) {
+  lval v;
+  v.type = LVAL_NUM;
+  v.num = x;
+  return v;
+}
+
+/* Creates new error type lval */
+lval lval_err(int x) {
+  lval v;
+  v.type = LVAL_ERR;
+  v.err = x;
+  return v;
+}
+
+void lval_print(lval v) {
+  switch (v.type) {
+  case LVAL_NUM: 
+    printf("%li", v.num);
+    break;
+
+  case LVAL_ERR:
+    if(v.err == LERR_DIV_ZERO) {
+      printf("Error: Division By Zero!");
+    } else if(v.err == LERR_BAD_OP) {
+      printf("Error: Invalid Operator!");
+    } else if(v.err == LERR_BAD_NUM) {
+      printf("Error: Invalid Number!");
+    }
+    break;
+  }
+}
+
+void lval_println(lval v) {
+  lval_print(v);
+  putchar('\n');
+}
+
 long exponential(long i, long n, long buf) {
   if (n == 0) {
     return 1; 
